be505970fcb4c33f16cd22dc91cca733b1991c92
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-03 08:20:38 +0800

    membuat endpoint untuk get data employee

--- server/index.js
+++ server/index.js
@@ -1,15 +1,41 @@
 import express from "express";
 import env from "dotenv";
-import cookieParser from "cookie-parser";
-import authRoute from "./routes/authRoute.js";
+import bcrypt from "bcrypt";
+import bodyParser from "body-parser";
+import pg from "pg";
+
 
 env.config();
-
 const app = express();
+const saltRounds = 12;
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+});
+db.connect();
 
-app.use(express.json());
-app.use(cookieParser());
-app.use("/api/auth", authRoute);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/employee", async (req, res) => {
+    try {
+        const query = await db.query("SELECT * FROM employee");
+        const result = query.rows;
+
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+app.post("/create-account", async (req, res) => {
+    const employeeId = req.body.employeeId;
+    const username = req.body.username;
+    const password = req.body.password;
+    const role = req.body.role;
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
