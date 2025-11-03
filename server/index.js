8bcf1f86867ab93785ba7f827c10354b492a7ace
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-03 11:53:08 +0800

    membuat endpoint untuk menambahkan supplier

--- server/index.js
+++ server/index.js
@@ -159,6 +159,32 @@ app.post("/add-stuff-brand", async (req, res) => {
     }
 });
 
+app.post("/add-supplier", async (req, res) => { 
+    const supplierName = req.body.supplierName;
+    const supplierContact = req.body.supplierContact;
+    const supplierAddress = req.body.supplierAddress;
+
+    if (!supplierName || !supplierContact || !supplierAddress) {
+        res.status(404).json({
+            status: 404,
+            message: "Missing required key: supplierName, supplierContact, supplierAddress"
+        });
+    }
+
+    try {
+        const query = await db.query("INSERT INTO supplier (supplier_name, supplier_contact, supplier_address) VALUES ($1, $2, $3) RETURNING *", [supplierName, supplierContact, supplierAddress]);
+        const result = query.rows[0];
+
+        res.status(200).json({
+            status: 200,
+            message: "OK",
+            data: result,
+        });
+    } catch (error) {
+        
+    }
+});
+
 // ACCOUNT
 app.post("/create-account", async (req, res) => {
     const employeeId = req.body.employeeId;
