df55c1e6c40901f97c683fc191e3063c11880cdc
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-03 11:20:15 +0800

    membuat endpoint untuk menambahkan employee

--- server/index.js
+++ server/index.js
@@ -30,17 +30,60 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// EMPLOYEE
 app.get("/employee", async (req, res) => {
     try {
         const query = await db.query("SELECT * FROM employee");
         const result = query.rows;
 
-        res.json(result);
+        res.status(200).json({
+            status: 200,
+            message: "OK",
+            data: result,
+        });
     } catch (err) {
         console.error(err);
     }
 });
 
+app.post("/add-employee", async (req, res) => { 
+    const employeeNik = req.body.employeeNik;
+    const employeeName = req.body.employeeName;
+    const employeeAddress = req.body.employeeAddress;
+
+    if (!employeeNik || !employeeName || !employeeAddress) {
+        res.status(404).json({
+            status: 400,
+            message: "Missing required key: employeeNik, employeeName, employeeAddress",
+        });
+    }
+
+    try {
+        const checkResult = await db.query("SELECT * FROM employee WHERE employee_nik = $1", [employeeNik]);
+
+        if (checkResult.rows.length > 0) {
+            res.status(404).json({
+                status: 404,
+                message: "NIK already used",
+            });
+        }
+        else
+        {
+            const query = await db.query("INSERT INTO employee (employee_nik, employee_name, employee_address) VALUES ($1, $2, $3) RETURNING *", [employeeNik, employeeName, employeeAddress]);
+            const result = query.rows[0];
+
+            res.status(200).json({
+                status: 200,
+                message: "OK",
+                data: result,
+            });
+        }
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+// ACCOUNT
 app.post("/create-account", async (req, res) => {
     const employeeId = req.body.employeeId;
     const username = req.body.username;
@@ -48,21 +91,27 @@ app.post("/create-account", async (req, res) => {
     const role = req.body.role;
 
     if (!employeeId || !username || !password || !role) {
-        return res.status(400).json({ status: "Error", message: "Missing required key: employeeId, username, password, role" });
+        return res.status(400).json({
+            status: 404,
+            message: "Missing required key: employeeId, username, password, role",
+        });
     }
 
     try {
         const checkResult = await db.query("SELECT * FROM employee_account WHERE username = $1", [username]);
 
         if (checkResult.rows.length > 0) {
-            res.status(404).json({status: "Error", message: "Username already used"});
+            res.status(404).json({
+                status: 404,
+                message: "Username already used"
+            });
         }
         else
         {
             bcrypt.hash(password, saltRounds, async (err, hash) => {
                 if (err)
                 {
-                    console.error("Error hashing password : ",err);
+                    console.error("Error hashing password : ", err);
                 }
                 else
                 {
@@ -70,14 +119,22 @@ app.post("/create-account", async (req, res) => {
                     const account = await query.rows[0];
 
                     req.login(account, (err) => {
-                        res.status(200).json({status: "OK" , message: "Create account success", data: [{employeeId: employeeId, username: username, password: password, role: role}]});
+                        res.status(200).json({
+                            status: 200,
+                            message: "Create account success",
+                            data: [{
+                                employeeId: employeeId,
+                                username: username,
+                                password: password,
+                                role: role
+                            }]
+                        });
                     });
                 }
             });
         }
     } catch (err) {
         console.error(err);
-        res.json({ message: err });
     }
 });
 
