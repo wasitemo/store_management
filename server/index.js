3dc6301ae575b1586eaf244b6be16c6f1da7c32e
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-05 12:36:19 +0800

    memperbarui code pada endpoint add-order agar dapat mengelola multiple item yang diorder

--- server/index.js
+++ server/index.js
@@ -28,6 +28,7 @@ app.use(
     })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -426,50 +427,75 @@ app.post("/add-order", async (req, res) => {
     const employeeId = parseInt(req.body.employeeId);
     const orderDate = req.body.orderDate;
     const payment = parseFloat(req.body.payment.replace(",", "."));
-    const stuffId = parseInt(req.body.stuffId);
-    const discountId = parseInt(req.body.discountId);
-    const warehouseId = parseInt(req.body.warehouseId);
-    const totalItem = parseInt(req.body.totalItem);
+    const items = req.body.items;
 
-    if (!customerId || !paymentMethodeId || !employeeId || !orderDate || !payment || !stuffId || !discountId || !warehouseId || !totalItem) {
+    if (!customerId || !paymentMethodeId || !employeeId || !orderDate || !payment || !items) {
         return res.status(404).json({
             status: 404,
-            message: "Missing required key: customerId, paymentMethodeId, employeeId, orderDate, payment, stuffId, discountId, warehouseId, totalItem"
+            message: "Missing required key: customerId, paymentMethodeId, employeeId, orderDate, payment, items[]"
         });
     }
 
     try {
         await db.query("BEGIN");
 
-        const stockQuery = await db.query("SELECT quantity FROM stock WHERE warehouse_id = $1 AND stuff_id = $2 FOR UPDATE", [warehouseId, stuffId]);
+        let totalPayment = 0;
 
-        if (stockQuery.rows.length === 0) {
-            throw new Error("Stuff not found");
+        for (const item of items) {
+            const stuffId = parseInt(item.stuffId);
+            const discountId = parseInt(item.discountId);
+            const warehouseId = parseInt(item.warehouseId);
+            let totalItem = parseInt(item.totalItem);
+            
+            if (!stuffId || !discountId || !warehouseId || !totalItem) {
+                throw new Error("Invalid item data in items[]");
+            }
+
+            const discountQuery = await db.query("SELECT discount_total FROM discount WHERE discount_id = $1", [discountId]);
+            const stuffQuery = await db.query("SELECT current_sell_price FROM stuff WHERE stuff_id = $1", [stuffId]);
+
+            const discount = discountQuery.rows[0].discount_total;
+            const stuffPrice = stuffQuery.rows[0].current_sell_price;
+
+            const discountPrice = stuffPrice * (discount / 100);
+            const totalPerItem = (stuffPrice - discountPrice) * totalItem
+
+            totalPayment += totalPerItem;
         }
 
-        const currentStock = stockQuery.rows[0].quantity;
-
-        if (currentStock < totalItem) {
-            throw new Error("Stock empty");
-        }
-
-        await db.query("UPDATE stock SET quantity = quantity - $1 WHERE warehouse_id = $2 AND stuff_id = $3", [totalItem, warehouseId, stuffId]);
-
-        const discountQuery = await db.query("SELECT discount_total FROM discount WHERE discount_id = $1", [discountId]);
-        const discount = discountQuery.rows[0].discount_total;
-        const stuffQuery = await db.query("SELECT current_sell_price FROM stuff WHERE stuff_id = $1", [stuffId]);
-        const stuffPrice = stuffQuery.rows[0].current_sell_price;
-        const discountPrice = stuffPrice * (discount / 100);
-        const totalPayment = stuffPrice - discountPrice;
         const remainingPayment = payment - totalPayment;
 
-        console.log(remainingPayment);
-        
+        if (remainingPayment < 0) {
+            throw new Error("You don't have enough money.");
+        }
+
         const orderQuery = await db.query("INSERT INTO customer_order (customer_id, payment_methode_id, employee_id, order_date, payment, total_payment, remaining_payment) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING order_id", [customerId, paymentMethodeId, employeeId, orderDate, payment, totalPayment, remainingPayment]);
-        
+            
         const orderId = orderQuery.rows[0].order_id;
 
-        await db.query("INSERT INTO customer_detail_order (stuff_id, discount_id, order_id, warehouse_id, total_item) VALUES ($1, $2, $3, $4, $5)", [stuffId, discountId, orderId, warehouseId, totalItem]);
+        for (const item of items)
+        {
+            const stuffId = parseInt(item.stuffId);
+            const discountId = parseInt(item.discountId);
+            const warehouseId = parseInt(item.warehouseId);
+            let totalItem = parseInt(item.totalItem);
+
+            const stockQuery = await db.query("SELECT quantity FROM stock WHERE warehouse_id = $1 AND stuff_id = $2 FOR UPDATE", [warehouseId, stuffId]);
+
+            if (stockQuery.rows.length === 0) {
+                throw new Error(`stuff id ${stuffId} not found in warehouse ${warehouseId}`);
+            }
+
+            const currentStock = stockQuery.rows[0].quantity;
+
+            if (currentStock < totalItem) {
+                throw new Error(`Insufficient stock for stuff_id ${stuffId}`);
+            }
+
+            await db.query("UPDATE stock SET quantity = quantity - $1 WHERE warehouse_id = $2 AND stuff_id = $3", [totalItem, warehouseId, stuffId]);
+
+            await db.query("INSERT INTO customer_detail_order (stuff_id, discount_id, order_id, warehouse_id, total_item) VALUES ($1, $2, $3, $4, $5)", [stuffId, discountId, orderId, warehouseId, totalItem]);
+        }
 
         await db.query("COMMIT");
 
