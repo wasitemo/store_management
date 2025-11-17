0bfa5842ceb93f9ad875433ff805a7ce1919fd7b
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-17 13:05:34 +0800

    menambahkan endpoint /update supplier

--- server/index.js
+++ server/index.js
@@ -346,7 +346,49 @@ app.post("/add-supplier", verifyToken, async (req, res) => {
     }
 });
 
-app.patch("/update-supplier/:supplier_id", async (req, res) => { });
+app.patch("/update-supplier/:supplier_id", verifyToken, async (req, res) => {
+    let reqId = parseInt(req.params.supplier_id);
+    let update = req.body;
+    let keys = Object.keys(update);
+    let fields = [
+        "supplier_name",
+        "supplier_contact",
+        "supplier_address"
+    ];
+    let invalidField = keys.filter(k => !fields.includes(k));
+
+    if (invalidField.length > 0) {
+        return res.status(400).json({
+            status: 400,
+            message: "Invalid field", invalidField
+        });
+    }
+
+    if (keys.length === 0) {
+        return res.status(400).json({
+            status: 400,
+            message: "No item updated"
+        });
+    }
+
+    let setQuery = keys.map((key, index) => `${key} = $${index + 1}`).join(",");
+    let values = Object.values(update);
+
+    try {
+        await db.query(`UPDATE supplier SET ${setQuery} WHERE supplier_id = $${keys.length + 1}`, [...values, reqId]);
+
+        return res.status(200).json({
+            status: 200,
+            message: "Sucess updated data"
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    }
+});
 
 // STUFF
 app.use("/add-stuff-category", async (req, res) => { 
