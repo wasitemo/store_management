acb84dfdb029a6d961aafd33f2e2a4f07bd9fd09
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-25 11:19:45 +0800

    menambahkan validasi jika data tidak ditemukan pada endpoint /employee/:employee_id

--- server/index.js
+++ server/index.js
@@ -247,17 +247,24 @@ app.get("/employee/:employee_id", verifyToken, async (req, res) => {
       "SELECT * FROM employee WHERE employee_id = $1",
       [reqId]
     );
-    let result = query.rows;
+    let result = query.rows[0];
+
+    if (query.rows.length === 0) {
+      return res.status(404).json({
+        status: 404,
+        message: "Data not found",
+      });
+    }
 
     return res.status(200).json({
       status: 200,
-      data: result[0],
+      data: result,
     });
   } catch (err) {
     console.log(err.message);
-    return res.status(400).json({
-      status: 400,
-      message: err.message,
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error",
     });
   }
 });
