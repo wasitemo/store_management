18d106dc1ddf84231da17002653bf56bed0a4dec
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-26 11:01:36 +0800

    menambahkan validasi ketika data tidak ditemukan dan mengubah status code pada endpoint get /payment-methods

--- server/index.js
+++ server/index.js
@@ -1999,15 +1999,22 @@ app.get("/payment-methods", verifyToken, async (req, res) => {
     let query = await db.query("SELECT * FROM payment_methode");
     let result = query.rows;
 
+    if (query.rows.length === 0) {
+      return res.status(404).json({
+        status: 404,
+        message: "Data not found",
+      });
+    }
+
     return res.status(200).json({
       status: 200,
       data: result,
     });
   } catch (err) {
     console.error(err);
-    return res.status(400).json({
-      status: 400,
-      message: err.message,
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error",
     });
   }
 });
