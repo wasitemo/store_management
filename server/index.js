770fc1bb48c903b717f5529651b9b9adbc6b59c3
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-26 14:22:22 +0800

    mengubah status code pada endpoint get /employee-accounts

--- server/index.js
+++ server/index.js
@@ -2852,8 +2852,8 @@ app.get("/employee-accounts", verifyToken, async (req, res) => {
     let result = query.rows;
 
     if (result.length === 0) {
-      return res.status(400).json({
-        status: 400,
+      return res.status(404).json({
+        status: 404,
         message: "Data not found",
       });
     }
@@ -2864,9 +2864,9 @@ app.get("/employee-accounts", verifyToken, async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    return res.status(400).json({
-      status: 400,
-      message: err.message,
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error",
     });
   }
 });
