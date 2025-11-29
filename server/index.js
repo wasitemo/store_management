ab635670033ab9d90bcf8116d07bcd692297b416
Navyrine <wahidadinugrohoo@gmail.com>
2025-11-29 17:37:56 +0800

    memperbaiki query pengambilan employee id pada enpoint patch /stuff/:stuff_id

--- server/index.js
+++ server/index.js
@@ -1398,7 +1398,7 @@ app.patch("/stuff/:stuff_id", verifyToken, async (req, res) => {
         FROM employee
         JOIN employee_account
         ON employee_account.employee_id = employee.employee_id
-        WHERE employee.employee_id = $1
+        WHERE employee_account.employee_account_id = $1
     `,
       [account.id]
     );
